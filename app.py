dbaa1fe1456d125540666963af0377304d13ec25
Soffivp <psvire@utpl.edu.ec>
2025-04-16 09:48:22 -0500

    leer archivo

--- app.py
+++ app.py
@@ -10,13 +10,15 @@ app = Flask(__name__)
 def home():
     actual = datetime.now()
     fecha_formateada = actual.strftime("%d, %B, %Y, %M, %H, %S")
-    return f'¡Hola, a todos. Bienvenidos! <b>{fecha_formateada}</b>
-    resp = requests.get('https://gist.githubusercontent.com/reroes/502d11c95f1f8a17d300ece914464c57/raw/872172ebb60e22e95baf8f50e2472551f49311ff/gistfile1.txt')
 
-resp.encoding
-'ISO-8859-1'
-resp.text
-'<!doctype html><html itemscope="" itemtype="h...'
+
+
+import requests
+
+r = requests.get("https://gist.githubusercontent.com/reroes/502d11c95f1f8a17d300ece914464c57/raw/872172ebb60e22e95baf8f50e2472551f49311ff/gistfile1.txt")
+with open("index.html", "wb") as f:
+    f.write(r.content)
+r.close()
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
